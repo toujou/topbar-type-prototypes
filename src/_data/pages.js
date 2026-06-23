a3c2dbf02297aed0fc4f0de8989e13bce251abea
LuisPato <lpato@dfau.de>
2026-06-23 08:06:42 +0200

    feat: Add images to the proto-list

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -1,4 +1,17 @@
 module.exports = [
-    { url: "/topbar-single-line/", label: "Topbar single line" },
-    { url: "/topbar-logo-on-top/", label: "Topbar logo on top" }
+    {
+        url: "/topbar-single-line/",
+        label: "Topbar single line",
+        mockupImage: "/assets/images/mockups/topbar-single-line.svg"
+    },
+    {
+        url: "/topbar-logo-on-top/",
+        label: "Topbar logo on top",
+        mockupImage: "/assets/images/mockups/topbar-logo-on-top.svg"
+    },
+    {
+        url: "/topbar-with-burger-button/",
+        label: "Topbar with burger button",
+        mockupImage: "/assets/images/mockups/topbar-with-burger-button.svg"
+    },
 ];

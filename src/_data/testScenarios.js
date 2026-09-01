b4f4569bed3949862405199b0bf5a6d6997fd3a0
LuisPato <lpato@dfau.de>
2026-09-01 12:02:32 +0200

    feat: Add new test scenarios data, improve scenarions-list hover styles Card: https://app.clickup.com/t/36621538/86cabecz7

--- src/_data/testScenarios.js
+++ src/_data/testScenarios.js
@@ -26,5 +26,33 @@ module.exports = [
             topbarType: "extra-actions-bar",
             navigationType: "dropdown"
         }
+    },
+    {
+        id: "scenario-c",
+        title: "Burger menu | logo on top",
+        description: "Render mobile topbar with the 'burger button' design and the 'full-height' navigation and the desktop 'logo on top' topbar with the 'panel' navigation",
+        breakpoint: "1024",
+        mobile: {
+            topbarType: "burger-button",
+            navigationType: "full-height"
+        },
+        desktop: {
+            topbarType: "logo-on-top",
+            navigationType: "panel"
+        }
+    },
+    {
+        id: "scenario-d",
+        title: "Burger menu | logo on top",
+        description: "Render mobile topbar with the 'burger button' design and the 'full-height' navigation and the desktop 'split' topbar with the 'dropdown' navigation",
+        breakpoint: "1024",
+        mobile: {
+            topbarType: "burger-button",
+            navigationType: "full-height"
+        },
+        desktop: {
+            topbarType: "split",
+            navigationType: "dropdown"
+        }
     }
 ];
